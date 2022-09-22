93a2f6e2d17185ce5290aa73dfbbd3eb8aa38a96
Phanupong <inrongpon.phanupong2@gmail.com>
2022-09-22 15:50:06 +0700

    add react dashboard  project

--- src/components/WorkCardData.js
+++ src/components/WorkCardData.js
@@ -1,6 +1,7 @@
 import pro1 from "../assets/project1.png";
 // import pro2 from "../assets/project2.png";
 // import pro3 from "../assets/project3.png";
+import pro5 from '../assets/project5.png';
 
 const ProjectCardData = [
   {
@@ -9,6 +10,13 @@ const ProjectCardData = [
     text: "Thriveing Facebook community and top-ranked podcast, Amy inspired a grounded, tangible and self-affirming sense of 'Wow! I really can do this' for over 100,000 online entrepreneur. Best-selling marketing courses",
     view: "https://my1stecommercewebsite.netlify.app/",
     source: "https://github.com/inrongpon22/myecommercewebsite1.git"
+},
+  {
+    imgsrc: pro5,
+    title: "React Admin Dashboard with syncfusion",
+    text: "React Admin Dashboard with syncfusion",
+    view: "https://react-admindashboard-syncfusion.netlify.app/",
+    source: "https://github.com/inrongpon22/React-admin-dashboard-with-synfusion"
 },
 //   {
 //     imgsrc: pro2,
